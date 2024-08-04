74b6ac1231e542b764c64fd00672a9788ca5b0ff
QuocTK <tranquocbn@gmail.com>
2024-08-04 17:18:51 +0700

    Update config devtool use eval source map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     // mode: 'production',
     mode: 'development',
+    devtool: "eval-source-map",
     entry: './src/index.ts',
     module: {
         rules: [
